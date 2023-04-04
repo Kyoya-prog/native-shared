de18dec448a0d959891aba8af8a8c19f72bcb321
kyoya <kyoyaprog@gmail.com>
2023-04-04 23:38:43 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "native"
+include(":android")
+include(":shared")
